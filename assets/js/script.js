46f2fd9073c5267d5f6dbd8aae8360df2f9dd781
Karan Dixit <dixitk941@gmail.com>
2024-07-13 16:47:30 +0530

    DARK MODE ANIMATION

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,137 @@
+/**
+ * Template Name: Mentor
+ * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+ * Updated: Jun 14 2024 with Bootstrap v5.3.3
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function() {
+  "use strict";
+
   /**
-    * Template Name: Mentor
-    * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-    * Updated: Jun 14 2024 with Bootstrap v5.3.3
-    * Author: BootstrapMade.com
-    * License: https://bootstrapmade.com/license/
-    */
-    
-  (function() {
-    "use strict";
-  
-    /**
-     * Apply .scrolled class to the body as the page is scrolled down
-     */
-    function toggleScrolled() {
-      const selectBody = document.querySelector('body');
-      const selectHeader = document.querySelector('#header');
-      if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-      window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-    }
-  
-    document.addEventListener('scroll', toggleScrolled);
-    window.addEventListener('load', toggleScrolled);
-  
-  
-    /**
-     * Preloader
-     */
-    const preloader = document.querySelector('#preloader');
-    if (preloader) {
-      window.addEventListener('load', () => {
-        preloader.remove();
-      });
-    }
-  
-    /**
-     * Scroll top button
-     */
-    let scrollTop = document.querySelector('.scroll-top');
-  
-    function toggleScrollTop() {
-      if (scrollTop) {
-        window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-      }
-    }
-    scrollTop.addEventListener('click', (e) => {
-      e.preventDefault();
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
     });
-  
-    window.addEventListener('load', toggleScrollTop);
-    document.addEventListener('scroll', toggleScrollTop);
-  
-    /**
-     * Animation on scroll function and init
-     */
-    function aosInit() {
-      AOS.init({
-        duration: 600,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
-    window.addEventListener('load', aosInit);
-  
-    /**
-     * Initiate glightbox
-     */
-    const glightbox = GLightbox({
-      selector: '.glightbox'
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
-  
-    /**
-     * Initiate Pure Counter
-     */
-    new PureCounter();
-  
-    /**
-     * Init swiper sliders
-     */
-    function initSwiper() {
-      document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-        let config = JSON.parse(
-          swiperElement.querySelector(".swiper-config").innerHTML.trim()
-        );
-  
-        if (swiperElement.classList.contains("swiper-tab")) {
-          initSwiperWithCustomPagination(swiperElement, config);
-        } else {
-          new Swiper(swiperElement, config);
-        }
-      });
-    }
-  
-    window.addEventListener("load", initSwiper);
-    const toggleButton = document.getElementById("dark-mode-toggle");
-    const toggleIcon = document.getElementById("dark-mode-icon");
-  
-    function updateIcon() {
-      if (document.documentElement.classList.contains("dark-mode")) {
-        toggleIcon.classList.remove("bi-moon");
-        toggleIcon.classList.add("bi-sun");
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
       } else {
-        toggleIcon.classList.remove("bi-sun");
-        toggleIcon.classList.add("bi-moon");
+        new Swiper(swiperElement, config);
       }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  const toggleButton = document.getElementById("dark-mode-toggle");
+  const toggleIcon = document.getElementById("dark-mode-icon");
+
+  function updateIcon() {
+    if (document.documentElement.classList.contains("dark-mode")) {
+      toggleIcon.classList.remove("bi-moon");
+      toggleIcon.classList.add("bi-sun");
+    } else {
+      toggleIcon.classList.remove("bi-sun");
+      toggleIcon.classList.add("bi-moon");
     }
-  
-    toggleButton.addEventListener("click", function() {
+  }
+
+  toggleButton.addEventListener("click", function() {
+    document.body.classList.add('color-transition');
+    setTimeout(() => {
       document.documentElement.classList.toggle("dark-mode");
       updateIcon();
-  
+
       // Save the user's preference to local storage
       if (document.documentElement.classList.contains("dark-mode")) {
         localStorage.setItem("dark-mode", "enabled");
       } else {
         localStorage.setItem("dark-mode", "disabled");
       }
-    });
-  
-    // On page load, check if dark mode was previously enabled
-    if (localStorage.getItem("dark-mode") === "enabled") {
-      document.documentElement.classList.add("dark-mode");
-      updateIcon();
-    }
-  
-  })();
+
+      setTimeout(() => {
+        document.body.classList.remove('color-transition');
+      }, 1000);
+    }, 500); // This should be shorter than the transition time to start the switch effect first
+  });
+
+  // On page load, check if dark mode was previously enabled
+  if (localStorage.getItem("dark-mode") === "enabled") {
+    document.documentElement.classList.add("dark-mode");
+    updateIcon();
+  }
+
+})();
