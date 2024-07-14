d1bad6c333abebee30785a9618c6fb236a56424f
Karan Dixit <dixitk941@gmail.com>
2024-07-14 22:51:37 +0530

    k

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Mentor
-* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-* Updated: Jun 14 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
